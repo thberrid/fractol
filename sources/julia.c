cd2c08b5a95e66ebf26165b7286b781d096f8b7d
thberrid <thberrid@student.42.fr>
2021-04-20 23:23:10 +0200

    julia set ok

---
+++ sources/julia.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   julia.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thberrid <thberrid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/12 09:56:52 by thberrid          #+#    #+#             */
+/*   Updated: 2021/04/12 09:57:04 by thberrid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fractol.h>
+
+int	julia_mouse(int x, int y, t_window *w)
+{
+	t_pixel		pixel;
+
+	if (w->state == LOCKED)
+		return (0);
+	w->state = LOCKED;
+	pixel.x = x;
+	pixel.y = y;
+	pixel_to_complex(&w->mouse_over, &pixel, w);
+	thread_draw(w);
+	return (0);
+}
+
+int julia(t_complex *zsrc, t_window *w)
+{
+	int			iterations;
+	int			index;
+	t_complex	z;
+	t_complex	z_sqr;
+
+	ft_memcpy(&z, zsrc, sizeof(t_complex));
+	index = 0;
+	iterations = 50 + w->zoom * 3;
+	while (index < iterations)
+	{
+		z_sqr.r = z.r * z.r;
+		z_sqr.i = z.i * z.i;
+		z.i = 2 * z.r * z.i + w->mouse_over.i;
+		z.r = z_sqr.r - z_sqr.i + w->mouse_over.r;
+		if (z_sqr.i + z_sqr.r > 4)
+			break ;
+		index += 1;
+	}
+	if (index == iterations)
+		return (0);
+	return (((index * 16 ) % 256 << 16) + ((index * 8 ) % 256 << 8) + ((index * 4 ) % 256));
+}
---
+++ sources/pixel.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pixel.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thberrid <thberrid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/12 10:53:49 by thberrid          #+#    #+#             */
+/*   Updated: 2021/04/12 10:53:59 by thberrid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fractol.h>
+
+t_complex	*pixel_to_complex(t_complex *dst, t_pixel *src, t_window *w)
+{
+
+	ft_bzero(dst, sizeof(t_complex));
+
+	dst->r = src->x * w->complex_plane.precision;
+	dst->i = src->y * w->complex_plane.precision * -1;
+	dst->r += w->complex_plane.minimum.r; 
+	dst->i += w->complex_plane.minimum.i; 
+	return (dst);
+}
+
+int 	pixel_to_addr(t_pixel *pixel, t_mlx_img *imgdata)
+{
+	int	addr;
+
+	addr = pixel->x * imgdata->pixel_size / 8;
+	addr += pixel->y * imgdata->line_size;
+	return (addr);
+}
